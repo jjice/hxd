01259b913e5dd55112ce12bc84ce6eb0a9b7fafe
jjice <joshuajallow1@gmail.com>
2025-12-05 15:21:51 +0100

    Add limit | offset

--- README.md
+++ README.md
@@ -8,6 +8,7 @@
 *   **Hex View**: Renders the bytes in hexadecimal notation (16 bytes per line).
 *   **ASCII View**: Outputs the printable characters. Non-printable characters are replaced with a period (`.`).
 *   **Simplicity**: No external dependencies and easy to compile.
+*   **Offset and Limit**: Set start byte to read an count of read bytes
 
 ## 🛠️ Compiling
 
@@ -36,6 +37,8 @@ The utility is run from the command line and takes exactly one argument.
 | `<-f file_path>` | The path to the file you want to inspect. This can be relative (e.g., `example.txt`) or absolute. | Yes      |
 | `<-a (on/off)>` | If you want a ASCII Table `-a off`, default is on | No      |
 | `<-h>` | Shows help | No      |
+| `<-o>` | Set start byte to read | No      |
+| `<-l>` | Set count of bytes to read  | No      |
 | `<-b (int)>` | Declare buffer size -> how many bytes are in a row. `-b 8`, default is 16 Bytes | No      |
 
 If no file path is provided, or if more than one argument is given, the program will print a usage message to the standard error stream and exit.
--- hxd.c
+++ hxd.c
@@ -3,6 +3,7 @@
 #include <stdbool.h>
 #include <errno.h>
 #include <string.h>
+#include <ctype.h>
 
 /** 
  * This documentation has been generated with ai. Use with caution!
@@ -19,6 +20,8 @@ typedef struct {
     char *filename;     // Path to the file to be read.
     int buff_size;      // The size of the buffer/line to display (e.g., 16 bytes).
     bool ascii;         // Flag to determine if the ASCII representation column should be shown.
+    size_t offset_read; // Bytes to skip until print
+    size_t limit_read;  // Count of bytes to be read/displayed
 } options;
 
 // bad characters
@@ -43,25 +46,47 @@ const unsigned char replace_with_dot[] = {
  * @param out_read Pointer to store the actual number of bytes read.
  * @param buff_size The maximum buffer size to read.
  * @param file The already opened FILE* handle.
+ * @param read_start Start Byte to read
  * @return const char* Pointer to the dynamically allocated buffer (must be freed by the caller).
  */
-const char *read_file_to_buffer(const char *filename, size_t *out_read, int buff_size, FILE *file) {
+const char *read_file_to_buffer(const char *filename, size_t *out_read, int buff_size, FILE *file, size_t read_start, size_t read_limit) {
     char *buffer = malloc(buff_size);
-    // STATIC OFFSET: Tracks the cumulative read position across all function calls.
-    static size_t offset = 0;
+    
+    // Comulative offset variable for file cursor
+    static bool offset_apply = false;
+    static size_t offset_continue = 0;
+    
+    // Read bytes from file
+    size_t read_buffer = 0;
+
+    // Apply start_offset
+    if (!offset_apply) offset_continue += read_start;
 
     if (!buffer) {
         perror("Cant allocate memory");
         exit(EXIT_FAILURE);
     }
 
-    // Repositions the file pointer based on the static offset.
-    fseek(file, offset, SEEK_SET);
+    // Current File cursor position
+    size_t current_pos = ftell(file);
 
-    size_t read_buffer = fread(buffer, 1, buff_size, file);
-    
+    // Absolut limit to read
+    size_t end_limit = read_start + read_limit;
+
+    // Remaining bytes to read
+    size_t remaining = 0;
+    if (current_pos < end_limit) {
+        remaining = end_limit - current_pos;
+    }
+
+    // If remaining is smaller than buff_size: read remaining else read buffsize
+    size_t to_read = remaining < (size_t)buff_size ? remaining : (size_t)buff_size;
+
+    // Read filecontent to buffer, output read bytes
+    read_buffer = fread(buffer, 1, to_read, file);
+
     // Updates the static offset for the next sequential read.
-    offset += read_buffer;
+    offset_continue += read_buffer;
 
     *out_read = read_buffer;
     return buffer;
@@ -75,22 +100,35 @@ const char *read_file_to_buffer(const char *filename, size_t *out_read, int buff
  *
  * @param filename The path to the file to be checked.
  */
-void check_file(const char *filename) {
-    FILE *fp = fopen(filename, "rb");
+void check_file(options *option) {
+    FILE *fp = fopen(option->filename, "rb");
 
     if(fp == NULL) {
         // File not found or permission denied.
-        perror("File not found");
+        perror("File not found / No permission to read");
         exit(EXIT_FAILURE);
     }
     else {
         // Check for zero size
         fseek(fp, 0, SEEK_END);
-        if(ftell(fp) == 0) {
+        size_t file_size = ftell(fp);
+        if(file_size == 0) {
             printf("File is empty"); // Prints error message
             fclose(fp);
             exit(EXIT_FAILURE);
         }
+        // Ceck if filesize is in range of offset till limit
+        if (file_size < option->offset_read + option->limit_read) {
+            printf("Filesize is out of range, check offset|limit");
+            fclose(fp);
+            exit(EXIT_FAILURE);
+        }
+        // Check if file limit has been placed, else EOF
+        if (option->limit_read == 0) {
+            fseek(fp, 0, SEEK_END);
+            option->limit_read = ftell(fp);
+        }
+
         // Close the file handle after successful check.
         fclose(fp);
     }
@@ -104,6 +142,9 @@ void check_file(const char *filename) {
 void print_hex(options *option){
     size_t bytes_read = 0;       // Bytes read in last read_file_to_buffer call.
     size_t addr_display = 0;     // Starting memory address to display for the current row.
+    
+    // Apply starting addr
+    addr_display += option->offset_read;
 
     // --- Header Formatting ---
     printf("\nHexdump for <%s>:\n\n", option->filename);
@@ -143,7 +184,7 @@ void print_hex(options *option){
     // --- Hex Output Loop ---
     do {
         // Reads data block by block using the helper function.
-        const char *buffer = read_file_to_buffer(option->filename, &bytes_read, option->buff_size, file);
+        const char *buffer = read_file_to_buffer(option->filename, &bytes_read, option->buff_size, file, option->offset_read, option->limit_read);
         if (!buffer) break; // Should only happen if malloc fails and exit is avoided.
         if (bytes_read == 0) {
             break; // End of file reached.
@@ -224,11 +265,16 @@ options *get_options(int argc, char *argv[]) {
     option->filename = NULL;
     option->buff_size = 16;
     option->ascii = true;
+    option->offset_read = 0;
+    option->limit_read = 0;
 
     // Help message string.
-    char *help = "Usage:\n       (-f) : <filename>"
-                 "\n    -b : <buffer> (default = 16)"
-                 "\n    -a : <on/off> (default = on)"
+    char *help = "Usage:"
+                 "\n  (-f) : <filename>"
+                 "\n    -b : Buffer <bytes>                  (default = 16)"
+                 "\n    -a : Show ASCII-Table <on/off>       (default = on)"
+                 "\n    -o : Offset to start reading <bytes> (default = 0)"
+                 "\n    -l : Limit to stop reading <bytes>   (default = EOF)"
                  "\n    -h : Show this info\n";
     
     // Loop through command line arguments starting from index 1.
@@ -298,6 +344,100 @@ options *get_options(int argc, char *argv[]) {
             }
         }
 
+        else if(strcmp(argv[x], "-o") == 0){
+            // Offset Flag
+            if (x + 1 >= argc) {
+                fprintf(stderr, "Error: -o requires an argument\n");
+                printf("%s", help);
+                exit(EXIT_FAILURE);
+            }
+            else {
+                errno = 0;
+                char *endptr;
+                long val = strtol(argv[x + 1], &endptr, 10);
+
+                // Check for existing argument
+                if (endptr == argv[x + 1]) {
+                    fprintf(stderr, "Error: -o requires a numeric value\n");
+                    printf("%s", help);
+                    exit(EXIT_FAILURE);
+                }
+
+                // Check for bad characters
+                if (*endptr != '\0' && !isspace((unsigned char)*endptr)) {
+                    fprintf(stderr, "Error: -o contains invalid characters\n");
+                    printf("%s", help);
+                    exit(EXIT_FAILURE);
+                }
+
+                // Check for overflow
+                if (errno == ERANGE) {
+                    fprintf(stderr, "Error: offset is too large or too small\n");
+                    exit(EXIT_FAILURE);
+                }
+
+                // Check for positiv value
+                if (val <= 0) {
+                    fprintf(stderr, "Error: offset must be positive\n");
+                    exit(EXIT_FAILURE);
+                }
+                // Set offset
+                option->offset_read = (size_t)val;
+                x++; // Skip the argument value.
+            }
+            
+        }
+
+        else if(strcmp(argv[x], "-l") == 0){
+            // Limit Flag
+            if (x + 1 >= argc) {
+                fprintf(stderr, "Error: -l requires an argument\n");
+                printf("%s", help);
+                exit(EXIT_FAILURE);
+            }
+            else {
+                errno = 0;
+                char *endptr;
+                long val = strtol(argv[x + 1], &endptr, 10);
+
+                // Check for existing argument
+                if (endptr == argv[x + 1]) {
+                    fprintf(stderr, "Error: -l requires a numeric value\n");
+                    printf("%s", help);
+                    exit(EXIT_FAILURE);
+                }
+
+                // Check for bad characters
+                if (*endptr != '\0' && !isspace((unsigned char)*endptr)) {
+                    fprintf(stderr, "Error: -l contains invalid characters\n");
+                    printf("%s", help);
+                    exit(EXIT_FAILURE);
+                }
+
+                // Check for overflow
+                if (errno == ERANGE) {
+                    fprintf(stderr, "Error: limit is too large or too small\n");
+                    exit(EXIT_FAILURE);
+                }
+
+                // Check for positiv value
+                if (val <= 0) {
+                    fprintf(stderr, "Error: limit must be positive\n");
+                    exit(EXIT_FAILURE);
+                }
+                // Check for higher value than offset
+                if (val < option->offset_read) {
+                    fprintf(stderr, "Error: limit must be greater than offset\n");
+                    exit(EXIT_FAILURE);
+                }
+
+                // Set offset
+                option->limit_read = (size_t)val;
+                x++; // Skip the argument value.
+            }
+            
+        }
+
         else if(strcmp(argv[x], "-h") == 0){
             // Help flag.
             printf("%s", help);
@@ -333,7 +473,7 @@ int main(int argc, char *argv[]) {
     options *option = get_options(argc, argv);
 
     // 2. Perform initial file existence and emptiness checks.
-    check_file(option->filename);
+    check_file(option);
 
     // 3. Execute the hex dump logic.
     print_hex(option);
